7510875caabb03cffb36ee3ec0f5b7e004bd650a
andriy-shymkiv <winamondezik@gmail.com>
2023-01-14 12:19:49 +0200

    ready-1

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Repo with initial setup
+          Repo with initial setup.
         </p>
         <a
           className="App-link"
